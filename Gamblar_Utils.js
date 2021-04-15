fa0bf619bc66189953b6ba508373f822cf03adec
DebdutChowdhury <debdut.chowdhury2018@gmail.com>
2021-04-15 12:53:55 +0530

    Added: Code is working for 20days for a month

--- Gamblar_Utils.js
+++ Gamblar_Utils.js
@@ -9,18 +9,19 @@ class Gamblar{
     }
 
     checkResult(){
-        let result = constant.STACK_AMOUNT;
-        while (result > (constant.STACK_AMOUNT / 2) && result < (constant.STACK_AMOUNT + (constant.STACK_AMOUNT / 2))){
-            if (this.randomCheck() == constant.BETTING_AMOUNT){
-                result = result + constant.BETTING_AMOUNT;
-                // console.log(`Score: ${result}`);
+        for (let day = 1; day <= constant.MONTHLY_DAYS; day++){
+            let result = constant.STACK_AMOUNT;
+            while (result > (constant.STACK_AMOUNT / 2) && result < (constant.STACK_AMOUNT + (constant.STACK_AMOUNT / 2))){
+                if (this.randomCheck() == constant.BETTING_AMOUNT){
+                    result = result + constant.BETTING_AMOUNT;
+                }
+                else{
+                    result = result - constant.BETTING_AMOUNT;
+                }
+                
             }
-            else{
-                result = result - constant.BETTING_AMOUNT;
-                // console.log(`Score: ${result}`);
-            }
-            console.log(`Game Result ${result}`);
-        }
+            console.log(`Game Result of ${day} day is : ${result}`);
+        }   
     }
 }
 
