0a7d538674724fd805a6f072ad49a6d553a20dde
DebdutChowdhury <debdut.chowdhury2018@gmail.com>
2021-04-19 08:56:51 +0530

    Added: calculate monthly total investment and stack amount

--- Gamblar_Utils.js
+++ Gamblar_Utils.js
@@ -1,8 +1,17 @@
 let constant = require('./Gambler_Constants')
-
+let monthCompute = require('./monthlyCount')
+let compute = new monthCompute(constant)
 class Gamblar{
-    constructor(){ }
-
+    // STACK_AMOUNT; BETTING_AMOUNT; MONTHLY_DAYS; monthly_investment; monthly_stake; gambler_stack_win; gambler_stack_loose
+    constructor(){
+        // this.STACK_AMOUNT = constant.STACK_AMOUNT;
+        // this.BETTING_AMOUNT = constant.BETTING_AMOUNT;
+        // this.MONTHLY_DAYS = constant.MONTHLY_DAYS;
+        // this.monthly_investment = constant.monthly_investment;
+        // this.monthly_stake = constant.monthly_stake;
+        // this.gambler_stack_win = constant.gambler_stack_win;
+        // this.gambler_stack_loose = constant.gambler_stack_loose;
+    }
     randomCheck(){
         let rand = Math.floor(Math.random() * 10) % 2 + 1;
         return rand;
@@ -11,17 +20,26 @@ class Gamblar{
     checkResult(){
         for (let day = 1; day <= constant.MONTHLY_DAYS; day++){
             let result = constant.STACK_AMOUNT;
-            while (result > (constant.STACK_AMOUNT / 2) && result < (constant.STACK_AMOUNT + (constant.STACK_AMOUNT / 2))){
+            while (result > constant.gambler_stack_loose && result < constant.gambler_stack_win){
                 if (this.randomCheck() == constant.BETTING_AMOUNT){
                     result = result + constant.BETTING_AMOUNT;
                 }
                 else{
                     result = result - constant.BETTING_AMOUNT;
                 }
-                
+            }            
+            // console.log(`Game Result of ${day} day is : ${result}`);
+            if (result > constant.gambler_stack_loose){
+                console.log(`Game win day ${day} amount : $${result}`);
             }
-            console.log(`Game Result of ${day} day is : ${result}`);
-        }   
+            else{
+                console.log(`Game loose day ${day} amount : &${result}`);
+            }
+            constant.monthly_investment += constant.STACK_AMOUNT;
+            constant.monthly_stake +=  result;
+        }
+        
+        compute.compute();
     }
 }
 
--- Gambler_Main.js
+++ Gambler_Main.js
@@ -1,2 +1,9 @@
 let gambler = require('./Gamblar_Utils')
 gambler.checkResult();
+// let constant = require('./Gambler_Constants')
+// class Main{
+    // call(){
+        // gambler().checkResult();
+    // }
+// }
+// new Main().call();
